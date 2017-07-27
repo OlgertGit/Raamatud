03f3737fcb0847c94726ff1737458fded8bbf578
Olgert <olgert.mihhaljov@yandex.ru>
2017-07-27 17:50:50 +0300

    Add project files.

---
+++ MvcRaamatud/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// Мы добавляем контроллер.
+namespace MvcMovie.Controllers
+{
+public class HelloWorldController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+        // Данный код для приветствие - Tere! (Один раз)
+    public ActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewBag.Message = "Tere! " + name;
+        ViewBag.NumTimes = numTimes;
+
+        return View();
+    }
+}
+}
---
+++ MvcRaamatud/Models/Movie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcMovie.Models
+{
+public class Movie
+{
+    public int ID
+    {
+        get;
+        set;
+    }
+        //Здесь я изменил название Title на Raamatu nimi.
+    [Display(Name = "Raamatu nimi")]
+    public string Title
+    {
+        get;
+        set;
+    }
+        //Здесь я изменил название ReleaseDate на Esmakordselt avaldatud.
+        [Display(Name = "Esmakordselt avaldatud")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime ReleaseDate
+    {
+        get;
+        set;
+    }
+        //здесь я изменил название Genre на Autor.
+    [Display(Name = "Autor")]
+    public string Genre
+    {
+        get;
+        set;
+    }
+        //Здесь я изменил название Price на Hind.
+    [Display(Name = "Hind")]
+    public decimal Price
+    {
+        get;
+        set;
+    }
+}
+
+public class MovieDBContext : DbContext
+{
+    public DbSet<Movie> Movies
+    {
+        get;
+        set;
+    }
+}
+}
